9a829e210d895b2de2f88b5fa73b6e496cbb7b0e
Eugenia <eugeniavillegasguerrero@gmail.com>
2025-03-18 19:30:29 +0100

    Correcion

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,8 @@ function resetearTurno() {
 function setearValor() {
     const setearHtmlElement = document.getElementById("turnoInput");
     if (setearHtmlElement !== null && setearHtmlElement !== undefined && setearHtmlElement instanceof HTMLInputElement) {
-        const setearHtmlInputElement = setearHtmlElement as HTMLInputElement;
-        const valor = setearHtmlInputElement.value;
+        const valor = setearHtmlElement.value;
+        turno = parseInt(valor);
         if (valor !== null && valor !== undefined) {
             actualizarVistaConValor(valor)
         }
@@ -33,27 +33,33 @@ function actualizarVista() {
 
 function actualizarVistaConValor(valor: string) {
     const turnoH1 = document.getElementById("turno");
-    turnoH1!.innerHTML = valor.padStart(2, "0");
+
+    if (turnoH1 !== null && turnoH1 !== undefined && turnoH1 instanceof HTMLHeadingElement) {
+        turnoH1.innerHTML = valor.padStart(2, "0");
+    }
 }
 
-const botonmenos = document.getElementById("botonmenos");
-const botonmas = document.getElementById("botonmas");
-const reset = document.getElementById("reset");
-const botonSetearValor = document.getElementById("setear");
+const cargarAplicacion = () => {
+    const botonmenos = document.getElementById("botonmenos");
+    const botonmas = document.getElementById("botonmas");
+    const reset = document.getElementById("reset");
+    const botonSetearValor = document.getElementById("setear");
 
-if (botonmas !== null && botonmas !== undefined) {
-    botonmas.addEventListener("click", () => incrementarTurno());
+    if (botonmas !== null && botonmas !== undefined && botonmas instanceof HTMLButtonElement) {
+        botonmas.addEventListener("click", () => incrementarTurno());
+    }
+
+    if (botonmenos !== null && botonmenos !== undefined && botonmenos instanceof HTMLButtonElement) {
+        botonmenos.addEventListener("click", () => decrementarTurno());
+    }
+
+    if (reset !== null && reset !== undefined && reset instanceof HTMLButtonElement) {
+        reset.addEventListener("click", () => resetearTurno());
+    }
+
+    if (botonSetearValor !== null && botonSetearValor !== undefined && botonSetearValor instanceof HTMLButtonElement) {
+        botonSetearValor.addEventListener("click", () => setearValor());
+    }
 }
 
-if (botonmenos !== null && botonmenos !== undefined) {
-    botonmenos.addEventListener("click", () => decrementarTurno());
-}
-
-if (reset !== null && reset !== undefined) {
-    reset.addEventListener("click", () => resetearTurno());
-}
-
-if (botonSetearValor !== null && botonSetearValor !== undefined) {
-    botonSetearValor.addEventListener("click", () => setearValor());
-}
-
+document.addEventListener('DOMContentLoaded', () => cargarAplicacion());
